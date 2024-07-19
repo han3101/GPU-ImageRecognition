71b864725637a587a64546b65069314891e4430e
Han <chinzonghan3101@berkeley.edu>
2024-07-19 07:58:45 +0000

    removed more cmake random stuff

--- ImageRecognition_test[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/home/azureuser/image_recognition/ImageRecognition_test[1]_tests.cmake")
-  include("/home/azureuser/image_recognition/ImageRecognition_test[1]_tests.cmake")
-else()
-  add_test(ImageRecognition_test_NOT_BUILT ImageRecognition_test_NOT_BUILT)
-endif()
